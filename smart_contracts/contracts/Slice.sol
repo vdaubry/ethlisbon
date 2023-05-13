a4db29f3e767370020c8b8c47f83f602463f6a62
Vincent Daubry <vdaubry@gmail.com>
2023-05-13 22:39:28 +0100

    add smart contract event SafeSet

--- smart_contracts/contracts/Slice.sol
+++ smart_contracts/contracts/Slice.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.17;
 contract Slice {
     mapping(address => address) eoaToSafe;
 
+    event SafeSet(address safe, address signer);
+
     function setSafe(address safe, address signer) public {
         eoaToSafe[signer] = safe;
+        emit SafeSet(safe, signer);
     }
 
     function getSafe(address signer) public view returns (address) {
